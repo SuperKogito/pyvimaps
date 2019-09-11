7a41995966cb659c2f28c2c9bac88f522eef1207
Ayoub Malek <15731839+SuperKogito@users.noreply.github.com>
2019-09-11 15:34:06 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -5,10 +5,15 @@ class Evaluation:
     def __init__(self, ground_truth, predictions):
         self.yt = ground_truth
         self.yp = predictions
-        self.yd = self.yt - self.yp
-    
+        try:
+            self.yd = self.yt - self.yp
+            self.n  = len(self.yd)
+        except ValueError:
+            print("Vectors lenghts do not match.")
+            raise
+            
     def mae(self):
-        return np.mean(self.yd)
+        return np.mean(np.abs(self.yd))
         
     def mse(self):
         return np.mean(self.yd**2)
@@ -17,10 +22,19 @@ class Evaluation:
         return np.sqrt(self.mse())
     
     def msle(self):
+        return np.mean(np.log((1 + self.yt) / (1 + self.yp))**2)
     
     def rae(self):
-    
+        return np.sum(np.abs(self.yt - self.yd)) / np.sum(np.abs(self.yt - np.mean(self.yt)))
+        
     def rse(self):
+        return np.sum(self.yt - self.yd)**2 / np.sum(self.yt - np.mean(self.yt))**2
     
     def r2(self):
+        return 1 - self.rse()
     
+    def mpe(self):
+        return 100 * np.mean(self.yd / self.yt)
+    
+    def mape(self):
+        return 100 * np.mean(np.abs(self.yd / self.yt))
